e076bb05e2a3d9da6fac29816f6cf0944cde7919
Pierre St Juste <ptony82@ufl.edu>
2012-05-17 12:32:29 -0400

    Updated android make file

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -7,13 +7,13 @@ LOCAL_MODULE := svpn
 
 LOCAL_CFLAGS += -D DROID_BUILD
 
-LOCAL_LDLIBS := -L/home/pierre/projects/external/openssl-android/libs/armeabi \
-                -lcrypto
+LOCAL_LDLIBS := -L. -lssl -lcrypto
 
 LOCAL_C_INCLUDES := /home/pierre/projects/external/openssl-android/include/ \
                    ../../src/
 
 LOCAL_SRC_FILES := ../../src/tap.c ../../src/peerlist.c ../../src/translator.c \
-                   ../../src/encryption.c ../../src/headers.c ../../src/svpn.c
+                   ../../src/encryption.c ../../src/headers.c ../../src/svpn.c \
+                   ../../src/dtls.c ../../src/bss_fifo.c
 
 include $(BUILD_EXECUTABLE)
