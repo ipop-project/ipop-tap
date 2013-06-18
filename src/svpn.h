a53be701c6a683b103dadab8e3c7410059b2e065
Pierre St Juste <ptony82@gmail.com>
2013-06-18 14:00:59 -0400

    Updated table size

--- src/svpn.h
+++ src/svpn.h
@@ -8,7 +8,7 @@
 #define BUFLEN 2048
 #define BUF_OFFSET 40 // Gives room to store the headers
 #define ID_SIZE 20
-#define TABLE_SIZE 10
+#define TABLE_SIZE 100
 #define MAXBUF 1024
 
 #define IPV6_ADDR_FILE "../ipv6_addr"
